ce31c3e6541d4b91dfeb1eafc033c2c5a9a7df5a
Joseph Scott <joseph@josephscott.org>
2020-08-26 16:57:15 -0600

    Start connecting FastRoute

--- src/server.php
+++ src/server.php
@@ -3,9 +3,11 @@ namespace Stratomatta;
 
 use Workerman\Worker;
 use Workerman\Protocols\Http\Response;
-use FastRoute\Dispatcher;
 
 class Server extends Worker {
+	protected $routes = [];
+	protected $dispatcher = false;
+
 	public function __construct(
 		string $socket_name = '',
 		array $context_options = []
@@ -14,10 +16,30 @@ class Server extends Worker {
 		$this->onMessage = [$this, 'onMessage'];
 	}
 
+	public function get( $path, $callback ) {
+		$this->routes['GET'][] = [$path, $callback];
+	}
+
 	public function onMessage( $connection, $request ) {
 	}
 
 	public function start() {
+		$this->dispatcher = \FastRoute\cachedDispatcher(
+			function( \FastRoute\RouteCollector $r ) {
+				foreach ( $this->routes as $method => $action ) {
+					foreach ( $action as $route ) {
+						// route[0] = path
+						// route[1] = callback
+						$r->addRoute( $method, $route[0], $route[1] );
+					}
+				}
+			},
+			[
+				'cacheFile' => '/tmp/stratomatta-route.cache',
+				'cacheDisabled' => true,
+			]
+		);
+
 		\Workerman\Worker::runAll();
 	}
 }
