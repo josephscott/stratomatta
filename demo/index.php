5084abb87e506be366d06824d15fa88a1c250a79
Joseph Scott <joseph@josephscott.org>
2024-03-23 22:48:40 -0600

    Fix add_route() call

--- demo/index.php
+++ demo/index.php
@@ -4,5 +4,5 @@ declare( strict_types = 1 );
 require __DIR__ . '/../vendor/autoload.php';
 
 $server = new Stratomatta\Server( 'http://localhost:4747' );
-$server->add_route( '/', __DIR__ . '/home.php' );
+$server->add_route( 'GET', '/', __DIR__ . '/home.php' );
 $server->start();
