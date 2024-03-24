f644167312fed256d38222001fe4232469ba026e
Joseph Scott <joseph@josephscott.org>
2024-03-23 22:12:58 -0600

    Start constructor

--- src/server.php
+++ src/server.php
@@ -9,4 +9,11 @@ use Workerman\Protocols\Http\Request;
 use Workerman\Protocols\Http\Response;
 use Workerman\Worker;
 
-class Server extends Worker {}
+class Server extends Worker {
+	public function __construct(
+		string $socket_name = 'http://localhost:7171',
+		array $context_options = []
+	) {
+		parent::__construct( $socket_name, $context_options );
+	}
+}
