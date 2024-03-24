468496b6f539abded34143507e81bc35a9aaab32
Joseph Scott <joseph@josephscott.org>
2024-03-23 22:34:58 -0600

    demo: startup

--- demo/index.php
+++ demo/index.php
@@ -2,3 +2,7 @@
 declare( strict_types = 1 );
 
 require __DIR__ . '/../vendor/autoload.php';
+
+$server = new Stratomatta\Server( 'http://localhost:4747' );
+$server->add_route( '/', __DIR__ . '/home.php' );
+$server->start();
