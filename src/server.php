074db913bb06970b06766f3b1cafed08f306afe5
Joseph Scott <joseph@josephscott.org>
2024-03-23 22:10:24 -0600

    Use references

--- src/server.php
+++ src/server.php
@@ -2,3 +2,11 @@
 declare( strict_types = 1 );
 
 namespace Stratomatta;
+
+use FastRoute\Dispatcher;
+use Workerman\Connection\TcpConnection;
+use Workerman\Protocols\Http\Request;
+use Workerman\Protocols\Http\Response;
+use Workerman\Worker;
+
+class Server extends Worker {}
