5459a463b5297becbf5da1939e4d7ffc858caa8b
Joseph Scott <joseph@josephscott.org>
2024-03-26 15:57:43 -0600

    Expand the demo home page

--- demo/index.php
+++ demo/index.php
@@ -4,5 +4,17 @@ declare( strict_types = 1 );
 require __DIR__ . '/../vendor/autoload.php';
 
 $server = new Stratomatta\Server( 'http://localhost:4747' );
-$server->add_route( 'GET', '/', __DIR__ . '/home.php' );
+$server->add_route( 'GET', '/', function ( ) {
+	$vars = get_defined_vars();
+
+	echo "<h2>echo</h2>\n<br />\n";
+
+	$out = "<pre>\n";
+	$out .= print_r( $vars, true );
+	$out .= "\n---\n";
+	$out .= print_r( $GLOBALS, true );
+	$out .= "</pre>\n";
+
+	return $out;
+} );
 $server->start();
